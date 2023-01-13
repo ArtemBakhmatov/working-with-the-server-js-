a91abad1e6c5973bbe1bb417fa30c57355442e84
Artem <ip.bakhmatov>
2023-01-13 18:07:39 +0300

    Как работать с сервером в JS. Часть 2 (get запрос fetch)

--- js/script.js
+++ js/script.js
@@ -2,66 +2,62 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     function request() {                      // запрос()
-        const request = new XMLHttpRequest(); // объект осинхронно дает общаться с сервером
-        request.open('GET', 'http://localhost:3000/people'); // Настройка запроса ('получить', 'адрес')
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); // (тип, формат)
-        request.send(); // отправить запрос
-        /*  Тут обработчик будет работать четыре раза подряд(0 -> 1 -> 2 -> 3 -> 4)
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status == 200) {
-                let data = JSON.parse(request.response); 
-                console.log(data);
-            } else {
-                console.error('Что-то пошло не так!');
-            }
-        });  */
-        
-        //Обработчик ('load') будет работать один раз при стадии 4, request.readyState необязятелен
-        request.addEventListener('load', () => { 
-            if (request.status == 200) {
-                let data = JSON.parse(request.response); 
-                console.log(data);
+        ///////////////// без функции getResource /////////////////////
+        /* fetch('http://localhost:3000/people')    // Запрос на указанный адрес
+            .then(data => data.json())              // Конвертируем из JSON в JS
+            .then(data => createCards(data))        // Выводим результат
+            .catch(err => console.error(err));      // Сработает только при ошибке
+             */
 
-                data.forEach(item => {
-                    let card = document.createElement('div');
-                    card.classList.add('card');
-
-                    let icon;
-                    if (item.sex === 'male') {
-                        icon = "icons/mars.png";
-                    } else {
-                        icon = "icons/female.png";
-                    }
-
-                    card.innerHTML = `
-                        <img src="${item.photo}" alt="">
-                        <div class="name">${item.name} ${item.surname}</div>
-                        <div class="sex">
-                            <img src=${icon} alt="male">
-                        </div>
-                        <div class="age">${item.age}</div>
-                    `;
-                    document.querySelector('.app').append(card);
-                });
-            } else {
-                console.error('Что-то пошло не так!');
-            }
-        });  
+        ///////////////// с функцией getResource /////////////////////
+        getResource('http://localhost:3000/people')
+            .then(data => createCards(data))
+            .catch(err => console.error(err));
 
         //this.remove(); // удаляется кнопка
         document.querySelector('button').remove(); // удаляется кнопка
     }
     document.querySelector('button').addEventListener('click', request, {"once": true});
+
+    async function getResource(url) { // Получить ресурс
+        const responce = await fetch(`${url}`);
+
+        if (!responce.ok) {  // если ошибка 
+            throw new Error(`Could not fetch ${url}, status: ${responce.status}`);
+        }
+        return await responce.json(); // возращаем асинхронный ответ в формате JS (Обычный объект)
+    }
+    
+    // async -> это означает что внутри функции есть асинхронная операция
+    // await -> это означате какая имеенно асинхронная операция
+    // throw -> инструкции после throw не будут выполнены
+
+    function createCards(response) {  // создать карточки
+        response.forEach(item => {
+            let card = document.createElement('div');
+            card.classList.add('card');
+
+            let icon;
+            if (item.sex === 'male') {
+                icon = "icons/mars.png";
+            } else {
+                icon = "icons/female.png";
+            }
+
+            card.innerHTML = `
+                <img src="${item.photo}" alt="">
+                <div class="name">${item.name} ${item.surname}</div>
+                <div class="sex">
+                    <img src=${icon} alt="male">
+                </div>
+                <div class="age">${item.age}</div>
+            `;
+            document.querySelector('.app').append(card);
+        });
+    }
+
+
     // {"once": true} -> обработчик сработае только один раз
 
-    // readystatechange - >отслеживает все стадии нашего запроса (0 -> 1 -> 2 -> 3 -> 4)
-    // load -> сработает тогда когда будет стадия 4
-    // 0 -> объект создан, open() не вызывался 
-    // 1 -> open(); был вызван
-    // 2 -> send(); был вызван
-    // 3 -> Загрузка , ResponseText
-    // 4 -> Операция полностью завершена
-    // status == 200 -> означает что всё отлично
-    // request.response -> Ответ от сервера, который был получен
-    // JSON.parse() -> парсит из JSON в JS
+     
 });
