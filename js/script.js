572cdb4c11e677b258318c1465af4153f0dab553
Artem <ip.bakhmatov>
2023-01-13 21:09:39 +0300

    Как работать с сервером в JS. Часть 3 (POST Запрос XMLHttpRequest в php)

--- js/script.js
+++ js/script.js
@@ -6,13 +6,22 @@ window.addEventListener('DOMContentLoaded', () => {
         event.preventDefault(); // отменить стандартное поведение браузера
 
         let formData = new FormData(form);      // объект: ключ -> значение
-        formData.append('id', Math.random());   // добавляем в объект новую пару (ключ: значение)
-        let object = {};                        // пустой объект
-        formData.forEach((value, key) => {
-            object[key] = value;
-        });
+        
+        const request = new XMLHttpRequest(); // объект осинхронно дает общаться с сервером
+        request.open('POST', './api.php'); // Настройка запроса ('отправить', 'адрес')
+        //request.setRequestHeader('Content-type', 'multipart/form-data'); // (тип, формат)
+        // Если есть new XMLHttpRequest() и new FormData(), то setRequestHeader не нужен
+        request.send(formData); // отправить запрос
 
-        axios.post('http://localhost:3000/people', object);
+        request.addEventListener('load', () => { 
+            if (request.status == 200) {
+                console.log(request.response);
+            } else {
+                console.error('Что-то пошло не так!');
+            }
+        });  
+
+        //axios.post('http://localhost:3000/people', object);
         
     }
     form.addEventListener('submit', (event) => request(event), {"once": true});
